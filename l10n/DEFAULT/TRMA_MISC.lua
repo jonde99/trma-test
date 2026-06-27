ac914c5300e22d4d68091d2ba1b861e8b02a5071
jonde99 <jonde99@yahoo.com>
2026-06-27 17:40:36 +0100

    Update TRMA_MISC.lua

--- l10n/DEFAULT/TRMA_MISC.lua
+++ l10n/DEFAULT/TRMA_MISC.lua
@@ -1,7 +1,7 @@
 _SETTINGS:SetPlayerMenuOff()
 _SETTINGS:SetImperial()
 
---important: the MessageToAll function was removed from Moose, we add it back here, else all other scripts will break.
+---important: the MessageToAll function was removed from Moose, we add it back here, else all other scripts will break.
 function MessageToAll( MsgText, MsgTime, MsgName )
   -- trace.f()
   MESSAGE:New( MsgText, MsgTime, "Message" ):ToCoalition( coalition.side.RED ):ToCoalition( coalition.side.BLUE )
